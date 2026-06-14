dfe8d314cf9863a9770ea88971bf8066dc2b180e
sakinesaw <sawsansakine@gmail.com>
2026-06-15 00:04:33 +0200

    feat: ajout de la puissance de clic et formatage des prix à deux décimales

--- PROJET GAME/script.js
+++ PROJET GAME/script.js
@@ -1,6 +1,8 @@
 const boutonCoffre = document.querySelector('#bouton-coffre');
 const montantGagne = document.querySelector('#argent')
 const boutiqueBouton = document.querySelector('#achat-PC')
+const nombrePc = document.querySelector('#nb-pc')
+const montantPc = document.querySelector('#prix-pc')
 
 // bloque le bouton de la boutique au démarrage
 boutiqueBouton.disabled = true; 
@@ -8,12 +10,21 @@ boutiqueBouton.disabled = true;
 // Variable qui stocke l'argent du joueur
 let butin = 0;
 
+// Variable valeur d'un clic en fonction des ameliorations
+let puissanceClic = 1; 
+
+// Variable compteur ordinateur 
+let nbrPC = 0;
+
+// Prix du pc initial
+let prixPC = 10;
+
 
 // Fonction qui vérifie le montant du butin actuel pour activer ou désactiver le bouton boutique
 
 function verifierBoutique() {
 
-        if (butin >= 10){
+        if (butin >= prixPC){
         boutiqueBouton.disabled = false;
     }
 
@@ -25,9 +36,9 @@ function verifierBoutique() {
 // actions lors du clic sur le coffre : gagner de l'argent
 
 boutonCoffre.addEventListener('click', () => {
-    butin = butin + 1 ;
+    butin = butin + puissanceClic ;
 
-    montantGagne.textContent = butin; 
+    montantGagne.textContent = butin.toFixed(2); 
 
     verifierBoutique()
 }); 
@@ -36,9 +47,22 @@ boutonCoffre.addEventListener('click', () => {
 
 boutiqueBouton.addEventListener('click', () =>{
 
-    butin = butin - 10 ; 
+    butin = butin - prixPC ; 
+    nbrPC = nbrPC + 1;
 
-    montantGagne.textContent = butin; 
+    if (nbrPC >= 5){
+        prixPC = prixPC * 1.5;
+    } 
+
+    else{
+        prixPC = prixPC
+    } 
+
+    montantGagne.textContent = butin.toFixed(2);
+    nombrePc.textContent = nbrPC;
+    montantPc.textContent = prixPC.toFixed(2); 
+
+    puissanceClic = puissanceClic + 1 ; 
 
     verifierBoutique()
 });
